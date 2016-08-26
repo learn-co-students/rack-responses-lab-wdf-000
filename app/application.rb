2c7e3b1c2127fc6f358ea6e5b2ec993e6f2ff4c2
Bibi Khadiza <limakhadiza@yahoo.com>
2016-08-26 12:03:33 -0400

    yus

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+
+
+  def call(env)
+    resp = Rack::Response.new
+    t = Time.now
+    if t.hour > 12
+      resp.write "Good Afternoon \n"
+    else
+      resp.write "Good Morning \n"
+    end
+    resp.finish
+  end
+
+
+
+
+
+
+
+
+end
